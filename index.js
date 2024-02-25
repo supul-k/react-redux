7d234b3bf1a042f818e85bd837683056c4fe3ac9
Supul Kalhara <supulkalhara7@gmail.com>
2024-02-25 11:38:42 +0530

    Refactor cake ordering and restocking logic

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { bindActionCreators } from '@reduxjs/toolkit';
 
 // const createstore = createStore;
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 const OrderCake = () => {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  };
+};
+
+const RestockCake = (qty) => {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   };
 };
 
@@ -19,7 +28,12 @@ const reducer = (state = initialState, action) => {
     case CAKE_ORDERED:
       return {
         ...state,
-        cakeCount: state.cakeCount - action.quantity,
+        cakeCount: state.cakeCount - action.payload,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        cakeCount: state.cakeCount + action.payload,
       };
     default:
       return state;
@@ -33,11 +47,18 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated state", store.getState());
 });
 
-store.dispatch(OrderCake({
-    type: CAKE_ORDERED,
-    quantity: 1,
-  }));
-store.dispatch(OrderCake());
-store.dispatch(OrderCake());
+// store.dispatch(OrderCake({
+//     type: CAKE_ORDERED,
+//     quantity: 1,
+//   }));
+// store.dispatch(OrderCake());
+// store.dispatch(OrderCake());
+// store.dispatch(RestockCake(5));
+
+const actions = bindActionCreators({ OrderCake, RestockCake }, store.dispatch);
+actions.OrderCake();
+actions.OrderCake();
+actions.OrderCake();
+actions.RestockCake(5);
 
 unsubscribe();
